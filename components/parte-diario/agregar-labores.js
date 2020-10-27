39ba94c9215cb60f8e72f033a93494ea8363cef2
GandresCoello18 <goyeselcoca@gmail.com>
2020-10-27 17:09:04 -0500

    fix screen parte diarios, eliminar y paginacion

--- components/parte-diario/agregar-labores.js
+++ components/parte-diario/agregar-labores.js
@@ -1,177 +1,236 @@
-import React, { useEffect, useState } from 'react';
-import { Picker } from '@react-native-picker/picker';
-import { View, Text, Button, StyleSheet, Alert } from 'react-native';
-import { insertar, db } from '../../db-local/config-db-local';
-import { MessageAlert } from '../pequenos/message';
+import React, {useEffect, useState} from 'react';
+import {Picker} from '@react-native-picker/picker';
+import {View, Text, Button, StyleSheet, Alert} from 'react-native';
+import {insertar, db} from '../../db-local/config-db-local';
+import {MessageAlert} from '../pequenos/message';
 
-export function AgregarLabores({ setLaboresAsignado }){
-    const [Labores, setLabores] = useState([]);
-    const [Cuadrillas, setCuadrillas] = useState([]);
-    const [Empleados, setEmpleados] = useState([]);
-    const [SelectEmpleado, setSelectEmpleado] = useState(null);
+export function AgregarLabores({
+  setLaboresAsignado,
+  LaboresAsignado,
+  setIsModal,
+}) {
+  const [Labores, setLabores] = useState([]);
+  const [Cuadrillas, setCuadrillas] = useState([]);
+  const [Empleados, setEmpleados] = useState([]);
+  const [SelectEmpleado, setSelectEmpleado] = useState(null);
 
-    const [Labor, setLabor] = useState('');
-    const [IsCuadrilla, setIsCuadrilla] = useState('');
-    const [Asignado, setAsignado] = useState([]);
+  const [Labor, setLabor] = useState('');
+  const [IsCuadrilla, setIsCuadrilla] = useState('');
+  const [Asignado, setAsignado] = useState([]);
 
-    useEffect( () => {
-        try {
-            const fetchDB = () => {
-                db.find({}, async function (err, docs) {
-                    if(err){
-                        Alert.alert(err.message);
-                    }
-                    setLabores(docs[0].Labores);
-                    setCuadrillas(docs[0].My_Cuadrilla);
+  useEffect(() => {
+    try {
+      const fetchDB = () => {
+        db.find({}, async function (err, docs) {
+          if (err) {
+            Alert.alert(err.message);
+          }
+          setLabores(docs[0].Labores);
+          setCuadrillas(docs[0].My_Cuadrilla);
 
-                    if(docs[0].My_Cuadrilla.length > 1){
-                        setIsCuadrilla('');
-                    }else{
-                        setIsCuadrilla(docs[0].My_Cuadrilla[0].Nombre);
-                        setEmpleados(docs[0].My_Cuadrilla[0].Empleados);
-                    }
-                });
-            }
+          if (docs[0].My_Cuadrilla.length > 1) {
+            setIsCuadrilla('');
+          } else {
+            setIsCuadrilla(docs[0].My_Cuadrilla[0].Nombre);
+            setEmpleados(docs[0].My_Cuadrilla[0].Empleados);
+          }
+        });
+      };
 
-            fetchDB();
-        } catch (error) {
-            Alert.alert(error.message);
-        }
-    },[db]);
-
-    const obtener_labor = (IdLabor) => {
-        const resul_labor = Labores.find(labor => labor.IdLabor === IdLabor);
-        return resul_labor.Nombre;
+      fetchDB();
+    } catch (error) {
+      Alert.alert(error.message);
     }
+  }, [db]);
 
-    const obtener_empleado = (IdEmpleado) => {
-        const result_empleado = Empleados.find(empleado => empleado.IdEmpleado === IdEmpleado);
-        return result_empleado.Nombre;
-    }
+  const obtener_labor = (IdLabor) => {
+    const resul_labor = Labores.find((labor) => labor.IdLabor === IdLabor);
+    return resul_labor.Nombre;
+  };
 
-    const obtener_empleado_asignado = (IdEmpleado) => {
-        const result = Asignado.find(asig  => asig.Empleado == IdEmpleado);
-        console.log(result);
-        return result;
-    }
-
-    const guardar_integrantes = () => {
-
-        if(SelectEmpleado && Labor){
-            if(obtener_empleado_asignado(SelectEmpleado) == undefined){
-                const tareas = [{
-                    Actividad: 'ningino',
-                    Hectarea: 'ninguno',
-                    Lote: 'ninguno',
-                }];
-                const asig = [];
-                asig.push({Empleado: SelectEmpleado, tareas});
-
-                setAsignado([...Asignado, ...asig]);
-                Alert.alert(`${obtener_empleado(SelectEmpleado)} fue guardado.`);
-            }else{
-                Alert.alert(`${obtener_empleado(SelectEmpleado)} ya fue guardado anteriormente.`);
-            }
-        }else{
-            Alert.alert(`Seleccione algun empleado de su cuadrilla: ( ${IsCuadrilla} )`);
-        }
-    }
-
-    const finalizar_asignaciones = () => {
-
-        const labores = {
-            labor: Labor,
-            Asignado: Asignado,
-        }
-
-        setLaboresAsignado(labores);
-    }
-
-    return(
-        <>
-            <Text style={styles.modalText}>Selecciona los datos necesarios.</Text>
-
-            {SelectEmpleado === null && (
-                <>
-                    <Text style={styles.label}>Labores:</Text>
-                    <View style={styles.select}>
-                        <Picker
-                            selectedValue={Labor}
-                            onValueChange={itemValue => setLabor(itemValue)}
-                        >
-                            {Labores.map((labor, index) => (
-                                <Picker.Item key={index} label={labor.Nombre} value={labor.IdLabor} />
-                            ))}
-                        </Picker>
-                    </View>
-                </>
-            )}
-
-            {IsCuadrilla ? (
-                <>
-                    <Text style={styles.label}>Mi cuadrilla: ( {IsCuadrilla} )</Text>
-
-                    <View style={styles.select}>
-                        <Picker
-                            selectedValue={SelectEmpleado}
-                            onValueChange={itemValue => setSelectEmpleado(itemValue)}
-                        >
-                            {Empleados.map((empleado, index) => (
-                                <Picker.Item key={index} label={empleado.Nombre + ' - ' + empleado.Apellido} value={empleado.IdEmpleado} />
-                            ))}
-                        </Picker>
-                    </View>
-                </>
-            ) : (
-                <>
-                    <Text style={[styles.label, {textAlign: 'center'}]}>Seleccione la cuadrilla</Text>
-
-                    <View style={styles.select}>
-                        <Picker
-                            selectedValue={IsCuadrilla}
-                            onValueChange={itemValue => {
-                                if(itemValue !== 'none'){
-                                    setIsCuadrilla(itemValue);
-                                    const ResultEmpleados = Cuadrillas.find(cuadrilla => cuadrilla.Nombre === itemValue);
-                                    setEmpleados(ResultEmpleados.Empleados);
-                                }
-                            }}
-                        >
-                            <Picker.Item label='** Seleccione la cuadrilla **' value='none' />
-                            {Cuadrillas.map((cuadrilla, index) => (
-                                <Picker.Item key={index} label={cuadrilla.Nombre} value={cuadrilla.Nombre} />
-                            ))}
-                        </Picker>
-                    </View>
-                </>
-            )}
-
-            {false && <MessageAlert background='#cdcdcd' content='cosa' />}
-
-            {SelectEmpleado && <Button title={`Guarda obrero en: ( ${obtener_labor(Labor)} )`} onPress={guardar_integrantes} />}
-            <Text>{'\n'}</Text>
-            {Asignado.length > 0 && <Button style={{ marginTop: 20 }} color='green' title={`Finalizar asignaciones`} onPress={finalizar_asignaciones} />}
-        </>
+  const obtener_empleado = (IdEmpleado) => {
+    const result_empleado = Empleados.find(
+      (empleado) => empleado.IdEmpleado === IdEmpleado,
     );
+    return result_empleado.Nombre;
+  };
+
+  const obtener_empleado_asignado = (IdEmpleado) => {
+    const result = Asignado.find((asig) => asig.Empleado == IdEmpleado);
+    return result;
+  };
+
+  const obtener_labores_asignados = (IdLabor) => {
+    return LaboresAsignado.find((labor) => labor.labor === IdLabor);
+  };
+
+  const guardar_integrantes = () => {
+    if (SelectEmpleado && Labor) {
+      if (obtener_labores_asignados(Labor) == undefined) {
+        if (obtener_empleado_asignado(SelectEmpleado) == undefined) {
+          const tareas = [
+            {
+              Actividad: 'ningino',
+              Hectarea: 'ninguno',
+              Lote: 'ninguno',
+            },
+          ];
+          const asig = [];
+          asig.push({Empleado: SelectEmpleado, tareas});
+
+          setAsignado([...Asignado, ...asig]);
+          Alert.alert(`${obtener_empleado(SelectEmpleado)} fue guardado.`);
+        } else {
+          Alert.alert(
+            `${obtener_empleado(
+              SelectEmpleado,
+            )} ya fue guardado anteriormente.`,
+          );
+        }
+      } else {
+        Alert.alert(
+          `La labor: ( ${obtener_labor(Labor)} ) ya fue asignada anteriomente`,
+        );
+      }
+    } else {
+      Alert.alert(
+        `Seleccione algun empleado de su cuadrilla: ( ${IsCuadrilla} )`,
+      );
+    }
+  };
+
+  const finalizar_asignaciones = () => {
+    const labores = {
+      labor: Labor,
+      Asignado: Asignado,
+    };
+
+    setLaboresAsignado([...LaboresAsignado, ...[labores]]);
+
+    console.log([...LaboresAsignado, ...[labores]]);
+    setIsModal(false);
+  };
+
+  return (
+    <>
+      <Text style={styles.modalText}>Selecciona los datos necesarios.</Text>
+
+      {SelectEmpleado === null && (
+        <>
+          <Text style={styles.label}>Labores:</Text>
+          <View style={styles.select}>
+            <Picker
+              selectedValue={Labor}
+              onValueChange={(itemValue) => {
+                if (obtener_labores_asignados(itemValue) == undefined) {
+                  setLabor(itemValue);
+                } else {
+                  Alert.alert(
+                    `La labor: ( ${obtener_labor(
+                      itemValue,
+                    )} ) ya fue asignada anteriomente`,
+                  );
+                }
+              }}>
+              {Labores.map((labor, index) => (
+                <Picker.Item
+                  key={index}
+                  label={labor.Nombre}
+                  value={labor.IdLabor}
+                />
+              ))}
+            </Picker>
+          </View>
+        </>
+      )}
+
+      {IsCuadrilla ? (
+        <>
+          <Text style={styles.label}>Mi cuadrilla: ( {IsCuadrilla} )</Text>
+
+          <View style={styles.select}>
+            <Picker
+              selectedValue={SelectEmpleado}
+              onValueChange={(itemValue) => setSelectEmpleado(itemValue)}>
+              {Empleados.map((empleado, index) => (
+                <Picker.Item
+                  key={index}
+                  label={empleado.Nombre + ' - ' + empleado.Apellido}
+                  value={empleado.IdEmpleado}
+                />
+              ))}
+            </Picker>
+          </View>
+        </>
+      ) : (
+        <>
+          <Text style={[styles.label, {textAlign: 'center'}]}>
+            Seleccione la cuadrilla
+          </Text>
+
+          <View style={styles.select}>
+            <Picker
+              selectedValue={IsCuadrilla}
+              onValueChange={(itemValue) => {
+                if (itemValue !== 'none') {
+                  setIsCuadrilla(itemValue);
+                  const ResultEmpleados = Cuadrillas.find(
+                    (cuadrilla) => cuadrilla.Nombre === itemValue,
+                  );
+                  setEmpleados(ResultEmpleados.Empleados);
+                }
+              }}>
+              <Picker.Item label="** Seleccione la cuadrilla **" value="none" />
+              {Cuadrillas.map((cuadrilla, index) => (
+                <Picker.Item
+                  key={index}
+                  label={cuadrilla.Nombre}
+                  value={cuadrilla.Nombre}
+                />
+              ))}
+            </Picker>
+          </View>
+        </>
+      )}
+
+      {false && <MessageAlert background="#cdcdcd" content="cosa" />}
+
+      {SelectEmpleado && (
+        <Button
+          title={`Guarda obrero en: ( ${obtener_labor(Labor)} )`}
+          onPress={guardar_integrantes}
+        />
+      )}
+      <Text>{'\n'}</Text>
+      {Asignado.length > 0 && (
+        <Button
+          style={{marginTop: 20}}
+          color="green"
+          title={`Finalizar asignaciones`}
+          onPress={finalizar_asignaciones}
+        />
+      )}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    modalText: {
-        marginBottom: 25,
-        textAlign: "center",
-        fontWeight: 'bold',
-    },
-    select: {
-        height: 50,
-        width: '100%',
-        borderRadius: 10,
-        borderWidth: 2,
-        borderColor: '#cdcdcd',
-        marginBottom: 10
-    },
-    label: {
-        fontWeight: 'bold',
-        textAlign: 'left',
-        paddingBottom: 15
-    }
+  modalText: {
+    marginBottom: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  select: {
+    height: 50,
+    width: '100%',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#cdcdcd',
+    marginBottom: 10,
+  },
+  label: {
+    fontWeight: 'bold',
+    textAlign: 'left',
+    paddingBottom: 15,
+  },
 });
--- screens/parteDiario.js
+++ screens/parteDiario.js
@@ -1,140 +1,297 @@
-import React, { useState, useEffect } from 'react';
-import {
-    Alert,
-    StyleSheet,
-    Text,
-    Button,
-    ScrollView,
-    View
-} from "react-native";
-import { MessageAlert } from '../components/pequenos/message';
-import { db } from '../db-local/config-db-local';
-import { ModalScreen } from '../components/modal/modal';
+import React, {useState, useEffect} from 'react';
+import {Alert, StyleSheet, Text, Button, ScrollView, View} from 'react-native';
+import {MessageAlert} from '../components/pequenos/message';
+import {db, insertar} from '../db-local/config-db-local';
+import {ModalScreen} from '../components/modal/modal';
 import * as Animatable from 'react-native-animatable';
 
-const ParteDiarioScreen = () => {
+const ParteDiarioScreen = ({navigation}) => {
+  const [isParteDiario, setIsParteDiario] = useState(true);
+  const [isModal, setIsModal] = useState(false);
+  const [isReload, setIsReload] = useState(false);
+  const [isRender, setIsRender] = useState('');
+  const [IndexDb, setIndexDb] = useState(1);
+  const [next_prev, setNext_Prev] = useState({next: true, prev: true});
+  const [MisPartesDiarios, setMisPartesDiarios] = useState([]);
+  const [Sectores, setSectores] = useState([]);
+  const [Labores, setLabores] = useState([]);
+  const [Cuadrillas, setCuadrillas] = useState([]);
+  const [LaboresAsignado, setLaboresAsignado] = useState([]);
 
-    const [isParteDiario, setIsParteDiario] = useState(true);
-    const [isModal, setIsModal] = useState(false);
-    const [isReload, setIsReload] = useState(false);
-    const [isRender, setIsRender] = useState('');
-    const [MisPartesDiarios, setMisPartesDiarios] = useState([]);
-    const [Sectores, setSectores] = useState([]);
-    const [LaboresAsignado, setLaboresAsignado] = useState([]);
+  useEffect(() => {
+    try {
+      const fetchDb = () => {
+        db.find({}, async function (err, docs) {
+          if (err) {
+            Alert.alert(err.message);
+          }
 
-    useEffect( () => {
-        try {
-            const fetchDb = () => {
-                db.find({}, async function (err, docs) {
-                    if(err){
-                        Alert.alert(err.message);
-                    }
+          if (IndexDb > 0) {
+            if (docs[IndexDb]) {
+              setIsParteDiario(false);
+              setMisPartesDiarios(docs[IndexDb].Mis_Parte_Diario);
+              setSectores(docs[0].Sectores);
+              setCuadrillas(docs[0].My_Cuadrilla);
+              setLabores(docs[0].Labores);
 
-                    if(docs[1]){
-                        setIsParteDiario(false);
-                        setMisPartesDiarios(docs[1].Mis_Parte_Diario);
-                        setSectores(docs[0].Sectores);
-                    }
-                });
+              if (IndexDb === 1) {
+                setNext_Prev({next: true, prev: true});
+              } else {
+                if (docs[IndexDb - 1]) {
+                  setNext_Prev({next: true, prev: false});
+                }
+              }
+
+              if (docs[IndexDb + 1]) {
+                setNext_Prev({next: false, prev: true});
+              }
             }
+          }
+        });
+      };
 
-            if(isReload){
-                setIsReload(false);
-            }
+      if (isReload) {
+        setIsReload(false);
+      }
 
-            fetchDb();
-        } catch (error) {
-            Alert.alert(error.message);
-        }
-    },[db, isReload]);
-
-    const obtenerSector = (IdSector) => {
-        if(Sectores.length > 0){
-            const Result_Sector =  Sectores.find(sector => sector.IdSector === IdSector);
-            return Result_Sector.Nombre + ' - ' + Result_Sector.Nombre_Hacienda;
-        }
+      fetchDb();
+    } catch (error) {
+      Alert.alert(error.message);
     }
+  }, [db, isReload]);
 
-    return (
-        <>
-            <View style={styles.container}>
-                <Text style={{ textAlign: 'center', fontWeight: 'bold', color: '#fff', fontSize: 20 }}>Parte diario</Text>
+  const delete_parte_diario = () => {
+    db.find({}, async function (err, docs) {
+      if (err) {
+        Alert.alert(err.message);
+      }
+      const dataBd = docs;
 
-                <Animatable.View 
-                    animation="fadeInUpBig"
-                    style={styles.footer}
-                >
-                    <ScrollView>
+      db.remove({}, {multi: true}, function (err, numRemoved) {
+        if (err) {
+          Alert.alert(err.message);
+        }
 
-                        {isParteDiario ? (
-                            <MessageAlert background='#cce5ff' content='Por el momento no existe ningun parte diario.' />
-                        ) : (
-                            <>
-                                {MisPartesDiarios.map(parte_diario => (
-                                    <View style={styles.header}>
-                                        <View style={styles.box}>
-                                            <Text>
-                                                <Text style={styles.label}>Sector: </Text>
-                                                {obtenerSector(parte_diario.sector)}
-                                            </Text>
-                                        </View>
-                                        <View style={styles.box}>
-                                            <Text>
-                                                <Text style={styles.label}>Tipo: </Text>
-                                                {parte_diario.tipo}
-                                            </Text>
-                                        </View>
-                                    </View>
-                                ))}
+        Alert.alert(`Se eliminaron ${numRemoved} registros guardados.`);
 
-                                <Button color='green' title='Agregar Labores' onPress={() => {
-                                    setIsModal(true); 
-                                    setIsRender('Agregar-labores');
-                                }} />
-                            </>
-                        )}
+        dataBd.splice(IndexDb, 1);
+        dataBd.map((data) => insertar(data));
+        navigation.navigate('SignInScreen');
+      });
+    });
+  };
 
-                        <View style={{ borderBottom: 2, borderBottomColor: '#cdcdcd', borderBottomWidth: 2, padding: 10 }}></View>
+  const obtener_labor = (IdLabor) => {
+    const resul_labor = Labores.find((labor) => labor.IdLabor === IdLabor);
+    return resul_labor.Nombre;
+  };
 
-                        <Button title='Crear Plantilla' onPress={() => {
-                            setIsModal(true); 
-                            setIsRender('Create-plantilla');
-                        }} />
-                    </ScrollView>
-                </Animatable.View>
-            </View>
+  const obtener_empleado = (IdEmpleado) => {
+    return Cuadrillas.map((cuadrilla) => {
+      const result_empleado = cuadrilla.Empleados.find(
+        (empleado) => empleado.IdEmpleado === IdEmpleado,
+      );
+      if (result_empleado !== undefined) {
+        return result_empleado.Nombre + ' - ' + result_empleado.Apellido;
+      }
+    });
+  };
 
-            <ModalScreen isModal={isModal} setIsModal={setIsModal} setIsReload={setIsReload} render={isRender} setLaboresAsignado={setLaboresAsignado} />
-        </>
-    );
-}
+  const obtenerSector = (IdSector) => {
+    if (Sectores.length > 0) {
+      const Result_Sector = Sectores.find(
+        (sector) => sector.IdSector === IdSector,
+      );
+      return (
+        Result_Sector.Nombre.trim() + ' - ' + Result_Sector.Nombre_Hacienda
+      );
+    }
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontWeight: 'bold',
+            color: '#fff',
+            fontSize: 20,
+          }}>
+          Parte diario
+        </Text>
+
+        <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+          <ScrollView>
+            {isParteDiario ? (
+              <MessageAlert
+                background="#cce5ff"
+                content="Por el momento no existe ningun parte diario."
+              />
+            ) : (
+              <>
+                {MisPartesDiarios.map((parte_diario) => (
+                  <>
+                    <Text
+                      style={styles.eliminar_parte_diario}
+                      onPress={delete_parte_diario}>
+                      Eliminar parte diario
+                    </Text>
+                    <View style={styles.header}>
+                      <View style={styles.box}>
+                        <Text>
+                          <Text style={styles.label}>Sector: </Text>
+                          {obtenerSector(parte_diario.sector)}
+                        </Text>
+                      </View>
+                      <View style={styles.box}>
+                        <Text>
+                          <Text style={styles.label}>Tipo: </Text>
+                          {parte_diario.tipo}
+                        </Text>
+                      </View>
+                    </View>
+                  </>
+                ))}
+
+                <Button
+                  color="green"
+                  title="Agregar Labores"
+                  onPress={() => {
+                    setIsModal(true);
+                    setIsRender('Agregar-labores');
+                  }}
+                />
+
+                {LaboresAsignado.map((labores, index) => (
+                  <>
+                    <Text>{'\n'}</Text>
+                    <Text>
+                      <Text style={styles.label}>Labor: </Text>
+                      {obtener_labor(labores.labor)}
+                    </Text>
+                    <View style={{padding: 10}} key={index}>
+                      <Text
+                        style={{
+                          textAlign: 'center',
+                          fontWeight: 'bold',
+                          padding: 10,
+                        }}>
+                        Empleados Asignados
+                      </Text>
+                      {labores.Asignado.map((asig, index) => (
+                        <Text
+                          style={{
+                            color: '#000',
+                            borderBottom: 2,
+                            borderBottomStyle: 'solid',
+                            borderBottomColor: '#cddcdcd',
+                            borderBottomWidth: 2,
+                          }}
+                          key={index}>
+                          {obtener_empleado(asig.Empleado)}
+                        </Text>
+                      ))}
+                    </View>
+                  </>
+                ))}
+              </>
+            )}
+
+            <View
+              style={{
+                borderBottom: 2,
+                borderBottomColor: '#cdcdcd',
+                borderBottomWidth: 2,
+                padding: 10,
+              }}></View>
+          </ScrollView>
+        </Animatable.View>
+      </View>
+
+      <View style={styles.btn_create_plantilla}>
+        <View style={[styles.header, {marginBottom: 10}]}>
+          <Button
+            title="Anterior"
+            color={next_prev.prev ? '#cdcdcd' : '#8FBF1D'}
+            disabled={next_prev.prev}
+            onPress={() => {
+              setIndexDb(IndexDb - 1);
+              setIsReload(true);
+            }}
+          />
+          <Button
+            title="Siguiente"
+            color={next_prev.next ? '#cdcdcd' : '#8FBF1D'}
+            disabled={next_prev.next}
+            onPress={() => {
+              setIndexDb(IndexDb + 1);
+              setIsReload(true);
+            }}
+          />
+        </View>
+        <Button
+          title="Crear Plantilla"
+          onPress={() => {
+            setIsModal(true);
+            setIsRender('Create-plantilla');
+          }}
+        />
+      </View>
+
+      <ModalScreen
+        isModal={isModal}
+        setIsModal={setIsModal}
+        setIsReload={setIsReload}
+        render={isRender}
+        setLaboresAsignado={setLaboresAsignado}
+        LaboresAsignado={LaboresAsignado}
+      />
+    </>
+  );
+};
 
 export default ParteDiarioScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        backgroundColor: '#009387'
-    },
-    footer: {
-        flex: Platform.OS === 'ios' ? 3 : 5,
-        backgroundColor: '#fff',
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingHorizontal: 20,
-        paddingVertical: 30
-    },
-    header: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    box: {
-        width: 160,
-        height: 100
-    },
-    label: {
-        fontWeight: 'bold',
-        fontSize: 16,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#009387',
+  },
+  footer: {
+    height: 600,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  box: {
+    width: 160,
+    height: 50,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  btn_create_plantilla: {
+    padding: 10,
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
+  },
+  eliminar_parte_diario: {
+    textAlign: 'center',
+    color: 'red',
+    padding: 5,
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: 'red',
+    marginBottom: 10,
+  },
 });
